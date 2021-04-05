acc5cd35766928bba56b58c357a09cfed7288e17
Ciwinforjsstuding <spid2001@yandex.ru>
2021-04-05 17:26:06 +0300

    delet the span in btn and change the js cod in cardActive

--- src/js/cardActive.js
+++ src/js/cardActive.js
@@ -5,16 +5,14 @@ const clearCard = () => {
     cards.forEach(node => {
       node.classList.remove("card-border-active");
       node.childNodes[9].classList.remove("card-btn-active");
-      node.childNodes[9].childNodes[1].classList.remove("card-btn-text-active");
     });
   }
-} 
+}
 
 cards.forEach(node => {
   node.addEventListener("click", () => {
     clearCard();
     node.classList.add("card-border-active");
     node.childNodes[9].classList.add("card-btn-active");
-    node.childNodes[9].childNodes[1].classList.add("card-btn-text-active");
   })
-})
+});
